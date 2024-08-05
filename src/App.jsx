8d5561749e31ad4a0a381e002945fb9c32a2ea64
JunW62 <sivia1990@gmail.com>
2024-08-04 20:15:20 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Items />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
